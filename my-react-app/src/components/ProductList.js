34aaa525ad603496ddba58a6b9bfbafa88fff861
adeeep14 <disconetct@gmail.com>
2022-10-30 11:36:21 +0700

    rotung,useEffect, dll

--- my-react-app/src/components/ProductList.js
+++ my-react-app/src/components/ProductList.js
@@ -1,15 +1,18 @@
+// Props merupakan salah satu cara untuk mengirimkan data dari komponen induk (Parent Components) ke komponen anak (Child Components).
 const ProductList = ({ products, deleteProduct }) => {
-    return (
-        <div>
-            <ul>
-                {products.map((product) => (
-                <li key={ product.id }> { product.title } - { product.price } 
-                    <button onClick={ () => deleteProduct(product.id) }>Delete</button>
-                </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {" "}
+            {product.title} - {product.price}
+            <button onClick={() => deleteProduct(product.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default ProductList
+export default ProductList;
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// import router dom
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/file" element={<Contact />} />
+    </Routes>
+  </BrowserRouter>
 );
-
